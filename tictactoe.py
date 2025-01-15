43d44d76bfb211be58ac35b262fe108166e43eec
Irakli <142930621+blitz-flex@users.noreply.github.com>
2025-01-15 21:26:06 +0400

    add player function

--- tictactoe.py
+++ tictactoe.py
@@ -22,7 +22,20 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    x_count = 0
+    O_count = 0
+
+    for row in board:
+        for cell in row:
+            if cell == X:
+                x_count += 0
+            elif cell == O:
+                o_count += 1
+    if  x_count > o_count:
+        return O
+    else:
+        return X
+
 
 
 def actions(board):
