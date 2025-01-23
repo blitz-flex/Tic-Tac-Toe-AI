38fb362fc54e85c9f341d320140080301bd61623
Irakli <142930621+blitz-flex@users.noreply.github.com>
2025-01-23 12:48:17 +0400

    modification max min _value

--- tictactoe.py
+++ tictactoe.py
@@ -198,10 +198,19 @@ def minimax(board):
 def max_value(board):
     if terminal(board):
         return utility(board)
+    v = -math.inf
+    for action in actions(board):
+        v = max(v, min_value(result(board, action)))
+    return v
+    
     
 def min_value(board):
     if terminal(board):
         return utility(board)
+    v = math.inf
+    for action in actions(board):
+        v = min(v, max_value(result(board, action)))
+    return v
 
         
     
